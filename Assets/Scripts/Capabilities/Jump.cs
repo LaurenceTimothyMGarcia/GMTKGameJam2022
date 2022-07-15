4a38e496caccaaa12cf3cc13056d2ec86f6217a9
kiennguyenGH <ktnguyen4647@gmail.com>
2022-07-15 14:21:17 -0700

    Add movement + jumping scripts

---
+++ Assets/Scripts/Capabilities/Jump.cs
@@ -0,0 +1,100 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Controller))]
+public class Jump : MonoBehaviour
+{
+    [SerializeField, Range(0f, 10f)] private float _jumpHeight = 3f;
+    [SerializeField, Range(0, 5)] private int _maxAirJumps = 0;
+    [SerializeField, Range(0f, 5f)] private float _downwardMovementMultiplier = 3f;
+    [SerializeField, Range(0f, 5f)] private float _upwardMovementMultiplier = 1.7f;
+    [SerializeField, Range(0f, 0.3f)] private float _coyoteTime = 0.2f;
+
+    private Controller _controller;
+    private Rigidbody2D _body;
+    private Ground _ground;
+    private Vector2 _velocity;
+    private int _jumpPhase;
+    private float _defaultGravityScale;
+    private float _jumpSpeed;
+    private float _coyoteCounter;
+    private bool _desiredJump;
+    private bool _onGround;
+    private bool _isJumping;
+
+    void Awake()
+    {
+        _body = GetComponent<Rigidbody2D>();
+        _ground = GetComponent<Ground>();
+        _controller = GetComponent<Controller>();
+
+        _defaultGravityScale = 1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        _desiredJump |= _controller.input.RetrieveJumpInput();
+    }
+    
+    private void FixedUpdate()
+    {
+        _onGround = _ground.OnGround;
+        _velocity = _body.velocity;
+
+        if (_onGround && _body.velocity.y == 0)
+        {
+            _jumpPhase = 0;
+            _coyoteCounter = _coyoteTime;
+            _isJumping = false;
+        }
+        else
+        {
+            _coyoteCounter -= Time.deltaTime;
+        }
+
+        if (_desiredJump)
+        {
+            _desiredJump = false;
+            JumpAction();
+        }
+
+        if (_controller.input.RetrieveJumpHoldInput() && _body.velocity.y > 0)
+        {
+            _body.gravityScale = _upwardMovementMultiplier;
+        }
+        else if (!_controller.input.RetrieveJumpHoldInput() || _body.velocity.y < 0)
+        {
+            _body.gravityScale = _downwardMovementMultiplier;
+        }
+        else if (_body.velocity.y == 0)
+        {
+            _body.gravityScale = _defaultGravityScale;
+        }
+
+        _body.velocity = _velocity;
+    }
+    private void JumpAction()
+    {
+        if (_coyoteCounter > 0f || _jumpPhase < _maxAirJumps && _isJumping)
+        {
+            if (_isJumping)
+            {
+                _jumpPhase += 1;
+            }
+
+            _coyoteCounter = 0;
+            _jumpSpeed = Mathf.Sqrt(-2f * Physics2D.gravity.y * _jumpHeight);
+            _isJumping = true;
+            
+            if (_velocity.y > 0f)
+            {
+                _jumpSpeed = Mathf.Max(_jumpSpeed - _velocity.y, 0f);
+            }
+            else if (_velocity.y < 0f)
+            {
+                _jumpSpeed += Mathf.Abs(_body.velocity.y);
+            }
+            _velocity.y += _jumpSpeed;
+        }
+    }
+}
