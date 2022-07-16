3d28728f3c0da16b1978f3bdd00c1a3be92b669c
Melvin <melvingit@gmail.com>
2022-07-15 20:17:37 -0700

    added stopwatch

---
+++ Assets/Stopwatch.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+
+public class Stopwatch : MonoBehaviour
+{
+    bool stopwatchActive = false;
+    float currentTime;
+    public TMP_Text currentTimeText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (stopwatchActive == true)
+        {
+            currentTime = currentTime + Time.deltaTime;
+        }
+        TimeSpan time = TimeSpan.FromSeconds(currentTime);
+        currentTimeText.text = time.ToString(@"mm\:ss\:fff");
+    }
+
+    public void startStopwatch()
+    {
+        stopwatchActive = true;
+    }
+
+    public void stopStopwatch()
+    {
+        stopwatchActive = false;
+    }
+}
