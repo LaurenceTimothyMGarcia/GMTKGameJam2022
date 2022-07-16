1a53fc1edff1407ed0418084d243836305770251
Christopher <programmerchristopher@gmail.com>
2022-07-15 20:37:20 -0700

    enemy prefabs follow player now, improved spawners

--- Assets/Scripts/EnemySpawner/Spawner.cs
+++ Assets/Scripts/EnemySpawner/Spawner.cs
@@ -17,14 +17,23 @@ public class Spawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(spawnEnemy(walkerInterval, walkerPrefab));
-        StartCoroutine(spawnEnemy(flyerInterval, flyerPrefab));
+        StartCoroutine(spawnEnemyFloor(walkerInterval, walkerPrefab));
+        StartCoroutine(spawnEnemyAir(flyerInterval, flyerPrefab));
     }
 
-    private IEnumerator spawnEnemy(float interval, GameObject enemy)
+    private IEnumerator spawnEnemyFloor(float interval, GameObject enemy)
     {
         yield return new WaitForSeconds(interval);
-        GameObject newEnemy = Instantiate(enemy, new Vector3(Random.Range(-5f, 5), Random.Range(-6f, 6f), 0), Quaternion.identity);
-        StartCoroutine(spawnEnemy(interval, enemy));
+        int ran = Random.Range(-1, 0);
+        if (ran == 0) ran = 1;
+        GameObject newEnemy = Instantiate(enemy, new Vector3(9.25f * ran, -2.75f, 0), Quaternion.identity);
+        StartCoroutine(spawnEnemyFloor(interval, enemy));
+    }
+
+    private IEnumerator spawnEnemyAir(float interval, GameObject enemy)
+    {
+        yield return new WaitForSeconds(interval);
+        GameObject newEnemy = Instantiate(enemy, new Vector3(Random.Range(-9.25f, 9.25f), Random.Range(-2.75f, 3.5f), 0), Quaternion.identity);
+        StartCoroutine(spawnEnemyAir(interval, enemy));
     }
 }
