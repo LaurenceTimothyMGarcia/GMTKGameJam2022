95c8d7b63bf67fa009f55d97094c60a7f274f95c
LaurenceTimothyMGarcia <LaurenceTimothyMGarcia@users.noreply.github.com>
2022-07-16 16:02:20 -0700

    all enemies dead works

--- Assets/Scripts/Weapons/EveryoneDead.cs
+++ Assets/Scripts/Weapons/EveryoneDead.cs
@@ -9,19 +9,43 @@ public class EveryoneDead : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //findEnemies = GameObject.FindGameObjectWithTag("Enemy");
+        findEnemies = GameObject.FindGameObjectsWithTag("Enemy");
 
         //For loop to destroy enemy
+        for (int i = 0; i < findEnemies.Length; i++)
+        {
+            KillEnemy(findEnemies[i]);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    void KillEnemy(Collider2D destroyEnemy)
+    void KillEnemy(GameObject enemy)
     {
-        Destroy(destroyEnemy);
+        Destroy(enemy);
     }
+
+    /*    Version if we want on command
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            KillEnemy();
+        }
+    }
+
+    void KillEnemy()
+    {
+        findEnemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+        //For loop to destroy enemy
+        for (int i = 0; i < findEnemies.Length; i++)
+        {
+            Destroy(findEnemies[i].gameObject);
+        }
+    }*/
 }
